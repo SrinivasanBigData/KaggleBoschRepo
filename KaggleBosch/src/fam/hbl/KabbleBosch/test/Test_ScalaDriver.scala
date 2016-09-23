26a3c8354a5b714e5b0710eae11ff265b8c226cc
LeGru <massimo.paolucci@gmail.com>
2016-09-23 09:04:23 +0200

    First attempt to make a test with spark
    Problems with testing equality of rows

--- KaggleBosch/src/fam/hbl/KabbleBosch/test/Test_ScalaDriver.scala
+++ KaggleBosch/src/fam/hbl/KabbleBosch/test/Test_ScalaDriver.scala
@@ -1,6 +1,7 @@
 package fam.hbl.KabbleBosch.test
 
 import collection.mutable.Stack  // Here only for example .... To Be Removed
+import org.apache.spark.sql.Row
 import org.scalatest._
 
 import fam.hbl.Kaggle.Bosch.SparkDriver
@@ -12,11 +13,16 @@ class Test_ScalaDriver extends FlatSpec with Matchers {
    *  To run the test it is required that there is a spark DataFrame that contains 
    *  some data.  
    */
-  
-  // create a spark session
-  val spark_session= SparkDriver.config_session()
-  // some data to create a 
-  
+	// create a spark session
+	val test_session= SparkDriver.config_session();
+
+  "Reading a file" should "return the correct values in the file" in {
+    val data= SparkDriver.load_data(test_session, 
+			"C:\\Users\\Massimo\\Code\\GitRepoS\\SparkBoschRepo\\KaggleBosch\\TestData\\labelled_data1.csv",
+			sep=";");
+    // get first row of 
+    val row= data.take(1)
+  }
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
